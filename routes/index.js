68c2b0f29d93629972b59573cc4712edb2071086
mtdwyer <michael.todd.dwyer@gmail.com>
2022-02-03 12:45:15 -0800

    Created Post, Get routes & req.params

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: "Michael's Express!!!!" });
 });
+
+router.get('/hello', function(req,res) {
+  res.send('Hello World from index')
+})
+
+router.get('/hello-array', function(req,res) {
+  res.send(['Hello', 'World', 'from', 'index'])
+})
+
+router.get('/hello-function', function(req,res) {
+  let newWord = 'Hello World'
+  res.send({
+    myWord: newWord
+  })
+})
+
+router.post('/post', function(req,res) {
+  res.send('success')
+})
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,29 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/hello', function(req,res) {
+  res.send('Hello World from users')
+})
+
+router.get('/name/:firstname',function(req,res){
+  console.log(req.params);  //  /users/name/Bob -> {firstname: "Bob"} -> req.firstname = "Bob"
+  res.send("Check your log")
+})
+
+router.get('/name/:firstname/:lastname/car/:make/:model',function(req,res){
+  console.log(req.params);  //  /users/name/Bob -> {firstname: "Bob"} -> req.firstname = "Bob"
+  let nameObj = {
+    category: "Name",
+    firstname: req.params.firstname,
+    lastname: req.params.lastname
+  }
+  let carObj = {
+    category: "Car",
+    make: req.params.make,
+    model: req.params.model
+  }
+
+  let finalArr = [nameObj, carObj]
+  res.send(finalArr)
+})
 module.exports = router;
