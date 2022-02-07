6547ee3f68cbf31f752c5a8d00abe79609bd3cf7
mtdwyer <michael.todd.dwyer@gmail.com>
2022-02-07 11:50:25 -0800

    created index/todo route, combined multiple await api callse

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios')
+
+const jsonAxios = axios.create({
+  baseURL: 'https://jsonplaceholder.typicode.com'
+})
+const apiAxios = axios.create({
+  baseURL: 'https://api.publicapis.org/entries'
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -26,8 +34,32 @@ router.post('/post', function(req,res) {
 })
 
 router.get('/api/:category', function(req, res){
-  console.log(req.params)
+  // console.log(req.params)
+  // apiAxios
   res.send(req.params)
+})
+
+router.get('/json/todos/:userid', function(req,res) {
+
+  const outputFunc = async () => {
+    try { 
+          let userResponse = await jsonAxios(`/users/${req.params.userid}`)
+          // console.log(userResponse.data);
+          let todoResponse = await jsonAxios(`/users/${req.params.userid}/todos`)
+          // console.log(todoResponse.data);
+          // res.send(todoResponse.data)
+          let filteredOutput = todoResponse.data.filter(e => e.completed === true)
+          let finalOutput = {
+            user: userResponse.data,
+            todos: filteredOutput
+          }
+          res.send(finalOutput)
+    } catch (error) {
+      console.log('!@-------ERROR!!!-------@!')
+      console.log(error.code)
+    }
+  }
+  outputFunc()
 })
 
 module.exports = router;
